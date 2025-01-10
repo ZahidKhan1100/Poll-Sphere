111e0f72c3489c76dbf07ce16bd4452e5ada6005
ZahidKhan1100 <kzahid416@gmail.com>
2025-01-10 17:54:09 +0100

    Migrations added and migrated

---
+++ database/migrations/2025_01_10_164352_create_questions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('survey_id')->constrained()->onDelete('cascade');
+            $table->string('question');
+            $table->enum('type', ['text', 'textarea', 'radio', 'checkbox', 'select', 'email', 'number', 'date', 'time']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
